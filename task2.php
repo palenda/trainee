9fd6b695adfe7ffae72207741056283f9f484d9c
Polina <94868256+palenda@users.noreply.github.com>
2022-10-21 15:45:09 +0300

    upd

--- task2.php
+++ task2.php
@@ -1,13 +1,25 @@
 <?php
 //Create a simple 'birthday countdown' script, the script counts the number of days left until the person’s birthday. Your script should determine the number of days based on the current date.
 
-function daysCounter(string $date)
+function Counter($birthday)
 {
-    $to_date = strtotime($date);
-    $today = strtotime(date('d-m-Y'));
-    $day_diff = $to_date - $today;
-    echo floor($day_diff/(60*60*24))." day to birthday left";
+    $parts = explode('-', $birthday, 2);
+    $birth_date = new DateTime(date('Y').'-'.$parts[1].'00:00:00');
+    $today = new DateTime('midnight today');
+    if ($birth_date < $today)
+    {
+        $birth_date->modify("+1 Year");
+    }
+
+    $diff = $birth_date->diff($today);
+
+    if($diff->days > 0)
+    {
+        echo "There are ".$diff->days." remaining until your birthday";
+    } else {
+        echo "Happy birthday!";
+    }
 }
 
-
+Counter("2001-10-19");
 
