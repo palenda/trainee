cd207cb9d3e1a7e4452af418c0e79b86bc56908c
Polina <94868256+palenda@users.noreply.github.com>
2022-10-21 15:45:30 +0300

    upd

--- task5.php
+++ task5.php
@@ -2,10 +2,20 @@
 //Given two integers A and B. Write a function that outputs all numbers from A to B inclusive, in ascending order if A < B, or in descending order otherwise. Use recursion.
 
 function recursion(int $i, int $n){
-    echo $i." ";
-    $i++;
-    if ($i <= $n){
-        recursion($i, $n);
-    } else echo "Error";
+    if ($i < $n) {
+        echo $i." ";
+        recursion($i + 1, $n);
+    }
+     else if ($n < $i)
+    {
+        echo $i." ";
+        recursion($i - 1, $n);
+    }
+     else if ($n == $i)
+     {
+         echo $i;
+         return 0;
+     }
 }
 
+recursion(11, -4);
